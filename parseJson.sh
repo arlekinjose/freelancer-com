eff7bad6e82bf7700b3630809ba7d8252e3b4756
Jose Medina <arlekinjose@gmail.com>
2020-01-18 09:39:53 -0400

    Parsing big json file

--- parseJson.sh
+++ parseJson.sh
@@ -11,6 +11,7 @@ runnerExist=`echo $var1 | jq --compact-output 'try .mc[].marketDefinition.runner
 if [ "$runnerExist" != "" ]
 then
   runners=`echo $var1 | jq --compact-output '.mc[].marketDefinition.runners'`
+
   for k in $(echo $runners | jq '. | keys | .[]')
   do
     name=`echo $var1 | jq '.mc[].marketDefinition.runners['$k'].name'`
@@ -25,4 +26,42 @@ then
 else
   echo "This json line does not have a runners array"
 fi
+
+mcExist=`echo $var1 | jq --compact-output 'try .mc[].marketDefinition?'`
+
+if [ "$mcExist" != "" ]
+then
+  bettingType=`echo $var1 | jq --compact-output '.mc[].marketDefinition.bettingType'`
+
+  if [ "$bettingType" != "null" ]
+  then
+    encryptName=`echo $bettingType | openssl rsautl -encrypt -inkey pub.pem -pubin -in - | base64 -w 0`
+    var1=`echo $var1 | jq --compact-output --arg encryptName "$encryptName" '.mc[].marketDefinition.bettingType=$encryptName'`
+  fi
+
+  marketType=`echo $var1 | jq --compact-output '.mc[].marketDefinition.marketType'`
+
+  if [ "$marketType" != "null" ]
+  then
+    encryptName=`echo $marketType | openssl rsautl -encrypt -inkey pub.pem -pubin -in - | base64 -w 0`
+    var1=`echo $var1 | jq --compact-output --arg encryptName "$encryptName" '.mc[].marketDefinition.marketType=$encryptName'`
+  fi
+fi
+
+  name=`echo $var1 | jq --compact-output '.mc[].marketDefinition.name'`
+
+  if [ "$name" != "null" ]
+  then
+    encryptName=`echo $name | openssl rsautl -encrypt -inkey pub.pem -pubin -in - | base64 -w 0`
+    var1=`echo $var1 | jq --compact-output --arg encryptName "$encryptName" '.mc[].marketDefinition.name=$encryptName'`
+  fi
+
+  eventName=`echo $var1 | jq --compact-output '.mc[].marketDefinition.eventName'`
+
+  if [ "$eventName" != "null" ]
+  then
+    encryptName=`echo $eventName | openssl rsautl -encrypt -inkey pub.pem -pubin -in - | base64 -w 0`
+    var1=`echo $var1 | jq --compact-output --arg encryptName "$encryptName" '.mc[].marketDefinition.eventName=$encryptName'`
+  fi
+
 echo $var1
